aa0844322920f090fb4dcbedd61d125bb1dedcc6
Gift1222 <giftchimwendo000@gmail.com>
2026-06-10 10:31:59 +0200

    AdmiPage

--- src/app/pages/admin/AdminDashboard.tsx
+++ src/app/pages/admin/AdminDashboard.tsx
@@ -172,7 +172,7 @@ export default function AdminDashboard() {
                     cx="50%"
                     cy="50%"
                     labelLine={false}
-                    label={(entry) => `${entry.name}: ${entry.value}`}
+                    label={(entry) => entry && entry.name ? `${entry.name}: ${entry.value}` : ''}
                     outerRadius={80}
                     fill="#8884d8"
                     dataKey="value"
